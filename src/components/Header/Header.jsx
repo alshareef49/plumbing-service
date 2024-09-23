30dca9450f99dbb46d5888cf8994411191aa17b4
Shareef Alam <alshareef4039@gmail.com>
2024-09-23 12:31:53 +0000

    added animation for toggler

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -42,46 +42,46 @@ export default function Header() {
                         </svg>
                     </button>
 
-                    {/* Navigation Links */}
+                    {/* Navigation Links with smooth animation */}
                     <div
-                        className={`${
-                            isOpen ? "block" : "hidden"
-                        } w-full lg:flex lg:w-auto`}
+                        className={`transition-max-height duration-500 ease-in-out overflow-hidden lg:flex lg:w-auto lg:order-1 ${
+                            isOpen ? "max-h-96" : "max-h-0"
+                        } w-full`}
                         id="mobile-menu"
                     >
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <NavLink
-                                    onClick={toggleMenu}
                                     to="/"
                                     className={({ isActive }) =>
                                         `${isActive ? "text-orange-700" : "text-gray-700"}
                                         block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
+                                    onClick={toggleMenu} // Close the menu when an item is clicked
                                 >
                                     Home
                                 </NavLink>
                             </li>
                             <li>
                                 <NavLink
-                                    onClick={toggleMenu}
                                     to="/about"
                                     className={({ isActive }) =>
                                         `${isActive ? "text-orange-700" : "text-gray-700"}
                                         block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
+                                    onClick={toggleMenu} // Close the menu when an item is clicked
                                 >
                                     About
                                 </NavLink>
                             </li>
                             <li>
                                 <NavLink
-                                    onClick={toggleMenu}
                                     to="/contact"
                                     className={({ isActive }) =>
                                         `${isActive ? "text-orange-700" : "text-gray-700"}
                                         block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
+                                    onClick={toggleMenu} // Close the menu when an item is clicked
                                 >
                                     Contact
                                 </NavLink>
